c3ef790b489b18765a3c755ca4f5c34f31442c01
Luis Duran <luis.duran@voltstudios.mx>
2023-06-27 23:32:09 -0600

    Contraseña agregada en profile activity

--- app/src/main/java/com/dualser/dadm/modulo4/tarea/ProfileActivity.kt
+++ app/src/main/java/com/dualser/dadm/modulo4/tarea/ProfileActivity.kt
@@ -3,7 +3,9 @@ package com.dualser.dadm.modulo4.tarea
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.dualser.dadm.R
 
 class ProfileActivity : AppCompatActivity() {
@@ -11,6 +13,8 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var tvFullName : TextView
     private lateinit var tvEmail : TextView
     private lateinit var tvSex : TextView
+    private lateinit var etPassword : EditText
+    private lateinit var btnEdit : Button
     private lateinit var btnLogout : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +28,8 @@ class ProfileActivity : AppCompatActivity() {
         tvFullName = findViewById(R.id.tvFullName)
         tvEmail = findViewById(R.id.tvEmail)
         tvSex = findViewById(R.id.tvSex)
+        etPassword = findViewById(R.id.etPassword)
+        btnEdit = findViewById(R.id.btnEdit)
         btnLogout = findViewById(R.id.btnLogout)
 
         val bundleProfile = intent?.getBundleExtra("EXTRA_BUNDLE")
@@ -32,10 +38,21 @@ class ProfileActivity : AppCompatActivity() {
             val lastName = it.getString("EXTRA_BUNDLE_LASTNAME")
             val email = it.getString("EXTRA_BUNDLE_EMAIL")
             val sex = it.getString("EXTRA_BUNDLE_SEX")
+            val password = it.getString("EXTRA_BUNDLE_PASSWORD")
 
             tvFullName.text = "$firstName $lastName"
             tvEmail.text = email
+            etPassword.setText(password)
             tvSex.text = sex.toString()
+        }
+
+        btnEdit.setOnClickListener {
+            if (etPassword.text.length < 8) {
+                Toast.makeText(this, "Su contraseña debe tener al menos 8 caracteres", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Su contraseña ha sido modificada", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         btnLogout.setOnClickListener {
