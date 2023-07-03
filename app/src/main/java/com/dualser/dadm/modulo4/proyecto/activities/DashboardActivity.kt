cabec8ebf928e4873b8ddabca7018bc11b39b0be
Luis Duran <luis.duran@voltstudios.mx>
2023-07-02 22:28:44 -0600

    Set up de proyecto. define Activities y Fragments

---
+++ app/src/main/java/com/dualser/dadm/modulo4/proyecto/activities/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.dualser.dadm.modulo4.proyecto.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dualser.dadm.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
